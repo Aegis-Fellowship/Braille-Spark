679d7fe256344c3071da2e5a67cabda3b43321b3
Mak <mohammedaqib@my.unt.edu>
2024-03-21 02:44:04 -0500

    adding dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './landing/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent},
-    { path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard', component: DashboardComponent}
 ];
