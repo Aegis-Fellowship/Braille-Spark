754e86d0576d168a51c0ddef38923124ba4939ff
Mak <mohammedaqib@my.unt.edu>
2024-03-22 11:56:07 -0500

    manageAccount

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes,} from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
 import { DashboardComponent } from './landing/dashboard/dashboard.component';
 import { ManageAccountComponent } from './components/manage-account/manage-account.component';
 
 export const routes: Routes = [
-    { path: '', component: ManageAccountComponent},
+    { path: '', component: LandingComponent},
     { path: 'dashboard', component: DashboardComponent},
-    {path: 'manageAccount', component: ManageAccountComponent}
+    { path: 'manageaccount', component: ManageAccountComponent}
 ];
