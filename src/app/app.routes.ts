7a0d3f9c1344dfc7ba6ce38585f1a69203549c2d
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2024-03-22 12:06:23 -0500

    transfer manage-account to navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes,} from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
 import { DashboardComponent } from './landing/dashboard/dashboard.component';
-import { ManageAccountComponent } from './components/manage-account/manage-account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent},
     { path: 'dashboard', component: DashboardComponent},
-    { path: 'manageaccount', component: ManageAccountComponent}
+    { path: 'navbar', component: NavbarComponent},
 ];
--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ManageAccountComponent } from './manage-account.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('ManageAccountComponent', () => {
-  let component: ManageAccountComponent;
-  let fixture: ComponentFixture<ManageAccountComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ManageAccountComponent]
+      imports: [NavbarComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(ManageAccountComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
