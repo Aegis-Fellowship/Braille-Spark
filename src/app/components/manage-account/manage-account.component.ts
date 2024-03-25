ce7a0f1c7d963099a9d191311846dbaaac154765
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2024-03-25 14:32:40 -0500

    styling and component structure

--- src/app/components/manage-account/manage-account.component.ts
+++ src/app/components/manage-account/manage-account.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { ChangeUsernameComponent } from './change-username/change-username.component';
+
 
 @Component({
   selector: 'app-manage-account',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, FormsModule, CommonModule],
+  imports: [RouterModule, FormsModule, CommonModule, NavbarComponent, ChangeUsernameComponent],
   templateUrl: './manage-account.component.html',
   styleUrl: './manage-account.component.sass'
 })
