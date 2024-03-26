0b0e26327deb71ffa648e4606f76a7afd9f37f90
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2024-03-25 19:07:42 -0500

    styling changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes,} from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
-import { DashboardComponent } from './landing/dashboard/dashboard.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ManageAccountComponent } from './components/manage-account/manage-account.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent},
-    { path: 'dashboard', component: DashboardComponent},
-    { path: 'navbar', component: NavbarComponent},
-    {path: 'manageAccount', component: ManageAccountComponent},
+    { path: 'manageAccount', component: ManageAccountComponent}
 ];
